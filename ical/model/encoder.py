c4c9e8b9116f8171659303b381cb59f4481d1105
anhtienpro25 <trunglh113@gmail.com>
2024-10-14 17:10:05 +0700

    commit

--- ical/model/encoder.py
+++ ical/model/encoder.py
@@ -10,6 +10,68 @@ from torch import FloatTensor, LongTensor
 
 from .pos_enc import ImgPosEnc
 
+class ChannelAttention(nn.Module):
+    def __init__(self, channels, reduction_rate=16):
+        super(ChannelAttention, self).__init__()
+        self.squeeze = nn.ModuleList([
+            nn.AdaptiveAvgPool2d(1),
+            nn.AdaptiveMaxPool2d(1)
+        ])
+        self.excitation = nn.Sequential(
+            nn.Conv2d(in_channels=channels,
+                      out_channels=channels // reduction_rate,
+                      kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=channels // reduction_rate,
+                      out_channels=channels,
+                      kernel_size=1)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # perform squeeze with independent Pooling
+        avg_feat = self.squeeze[0](x)
+        max_feat = self.squeeze[1](x)
+        # perform excitation with the same excitation sub-net
+        avg_out = self.excitation(avg_feat)
+        max_out = self.excitation(max_feat)
+        # attention
+        attention = self.sigmoid(avg_out + max_out)
+        return attention * x
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels=2,
+            out_channels=1,
+            kernel_size=kernel_size,
+            padding=kernel_size // 2
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # mean on spatial dim
+        avg_feat    = torch.mean(x, dim=1, keepdim=True)
+        # max on spatial dim
+        max_feat, _ = torch.max(x, dim=1, keepdim=True)
+        feat = torch.cat([avg_feat, max_feat], dim=1)
+        out_feat = self.conv(feat)
+        attention = self.sigmoid(out_feat)
+        return attention * x
+
+class CBAM(pl.LightningModule):
+    def __init__(self, channels, reduction_rate=16, kernel_size=7):
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttention(channels,
+                                                      reduction_rate)
+        self.spatial_attention = SpatialAttention(kernel_size)
+
+    def forward(self, x):
+        out = self.channel_attention(x)
+        out = self.spatial_attention(out)
+
+        return out
 
 # DenseNet-B
 class _Bottleneck(nn.Module):
@@ -45,11 +107,13 @@ class _SingleLayer(nn.Module):
         self.conv1 = nn.Conv2d(
             n_channels, growth_rate, kernel_size=3, padding=1, bias=False
         )
+        self.cbam = CBAM(channels=growth_rate, reduction_rate=16, kernel_size=7)
         self.use_dropout = use_dropout
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
         out = self.conv1(F.relu(x, inplace=True))
+        out = self.cbam(out)
         if self.use_dropout:
             out = self.dropout(out)
         out = torch.cat((x, out), 1)
